3e7258e6313067a90c170c0bfda88a0a89cdfe07
JohnMDoll <johnmdoll3@gmail.com>
2022-11-30 09:30:38 -0600

    game logic good, just have to fix rerendering

--- src/gametest/GameRunner.js
+++ src/gametest/GameRunner.js
@@ -4,96 +4,107 @@ let i = 0 // i & j for row & column id/addresses assigned to each cell
 let j = 0
 
 export const GameRunning = ({ started, allCellReferences }) => { //Intended to make an html cell with all the necessary unique attributes
-    const [gridLength, setGridLength] = useState(10) //how big the playing grid will be (it's a square, so just need 1 dimension)
-    let allIds = []
     let allCells = allCellReferences
-    //on game start, repopulating allIds array with cell objects for rerendering in return statement?
-    // activeCellArray.map(activeCell => {
-    //     const thisCell = {}
-    //     thisCell.address = activeCell
-    //     thisCell.status = true
-    //     return allIds.push(thisCell)
-    // })
-    // deadCellArray.map(deadCell => {
-    //     const thisCell = {}
-    //     thisCell.address = deadCell
-    //     thisCell.status = false
-    //     return allIds.push(thisCell)
-    // })
+
+    allCellReferences.map((cell) => { //pull x/y coordinates for each cell for neighbor checking
+        let [xCoord, yCoord] = cell.address.split("--")
+        cell.xCoord = parseInt(xCoord)
+        cell.yCoord = parseInt(yCoord)
+    })
+
+    const checkTheNeighborhood = () => {
+        let cellsCopy = [...allCellReferences] //maintain cell state during neighbor calculations and update all at once after calcs finish
+        //initialize neighbors count
+        cellsCopy.map(cell => { cell.neighbors = 0 })
+
+        cellsCopy.map(cell => {
+            // console.log(cell.xCoord)
+            // console.log(cell.yCoord)
+            const neighborUL = allCellReferences.find(neighborCell => {
+                return neighborCell.xCoord === (cell.xCoord - 1) && neighborCell.yCoord === (cell.yCoord - 1)
+            })
+            const neighborML = allCellReferences.find(neighborCell => {
+                return neighborCell.xCoord === (cell.xCoord - 1) && neighborCell.yCoord === (cell.yCoord)
+            })
+            const neighborBL = allCellReferences.find(neighborCell => {
+                return neighborCell.xCoord === (cell.xCoord - 1) && neighborCell.yCoord === (cell.yCoord + 1)
+            })
+            const neighborBM = allCellReferences.find(neighborCell => {
+                return neighborCell.xCoord === (cell.xCoord) && neighborCell.yCoord === (cell.yCoord - 1)
+            })
+            const neighborUM = allCellReferences.find(neighborCell => {
+                return neighborCell.xCoord === (cell.xCoord) && neighborCell.yCoord === (cell.yCoord + 1)
+            })
+            const neighborBR = allCellReferences.find(neighborCell => {
+                return neighborCell.xCoord === (cell.xCoord + 1) && neighborCell.yCoord === (cell.yCoord - 1)
+            })
+            const neighborMR = allCellReferences.find(neighborCell => {
+                return neighborCell.xCoord === (cell.xCoord + 1) && neighborCell.yCoord === (cell.yCoord)
+            })
+            const neighborUR = allCellReferences.find(neighborCell => {
+                return neighborCell.xCoord === (cell.xCoord + 1) && neighborCell.yCoord === (cell.yCoord + 1)
+            })
+            //putting all of those found neighbors into this array and then adding count of alive ones to the current cell's neighbor count
+            let neighborArray = [neighborBL, neighborBM, neighborBR, neighborML, neighborMR, neighborUL, neighborUM, neighborUR]
+            // console.log(neighborArray)
+
+            neighborArray.map(each => {
+                if (each?.status === true) {
+                    cell.neighbors++
+                } else {
+                    cell.neighbors += 0
+                }
+            })
+        })
+
+        allCellReferences = cellsCopy
+
+        allCellReferences.map(currentGenCell => {
+            if (currentGenCell.status === true && currentGenCell.neighbors === 2 || currentGenCell.neighbors === 3) {
+                currentGenCell.status = true
+            } else if (currentGenCell.status === true && currentGenCell.neighbors < 2 || currentGenCell.neighbors > 3) {
+                currentGenCell.status = false
+            } else if (currentGenCell.neighbors === 3) {
+                currentGenCell.status = true
+            }
+            console.log(currentGenCell.address)
+            console.log(currentGenCell.status)
+            console.log(currentGenCell.neighbors)
+        })
+    }
 
     useEffect(() => {
         const interval = setInterval(() => {
-            console.log('Got intervalled on')
-        }, 10000)
+            checkTheNeighborhood()
+            rerendererer()
+            // console.log(allCellReferences.filter(cell => cell.status === false))
+            // console.log(allCellReferences.map(cell => [cell.address, cell.neighbors, cell.status]))
+        }, 100000)
         return () => clearInterval(interval)
     },
         []
     )
-    
-    /*leftover while for setting up initial layout from cellFactory.js copy
-    // while (i < gridLength) { //one loop to build each row's ids
-    //     i++
-    //     while (j < gridLength) { // another loop to build each column cell's id's within each row
-    //         j++
-    //         allIds.push(`${i}--${j}`)
-    //     }
-    //     j = 0
-     }*/
 
-    //make a function that computes what each cell's status is on next generation render
-    // const NeighborChecker = () => {
+    const rerendererer = () => {
+        return <>
+            {
+                allCells.map(cell => {
+                    return <div key={cell.address}
+                        onClick={(evt) => {
+                            console.log(`${cell.address} clicked on`)
+                        }}
+                        className={cell.status === true ? "active" : cell.status === false ? "dead" : "initialCell"} id={cell.address} value="">
+                        {cell.address}
+                    </div>
+                })
+            }
+        </>
+    }
 
-    //     let aliveSiblings = 0;
-    //     aliveSiblings += (x, y + 1) ? 1 : 0;
-    //     aliveSiblings += (x + 1, y + 1) ? 1 : 0;
-    //     aliveSiblings += (x + 1, y) ? 1 : 0;
-    //     aliveSiblings += (x + 1, y - 1) ? 1 : 0;
-    //     aliveSiblings += (x, y - 1) ? 1 : 0;
-    //     aliveSiblings += (x - 1, y - 1) ? 1 : 0;
-    //     aliveSiblings += (x - 1, y) ? 1 : 0;
-    //     aliveSiblings += (x - 1, y + 1) ? 1 : 0;
-
-    //     const isAlive = (x, y);
-
-    //     // Cell live rules
-    //     if (isAlive) {
-    //         if (aliveSiblings >= 2 && aliveSiblings <= 3) return true;
-    //     }
-    //     else if (aliveSiblings === 3) return true;
-
-    //     return false;
-    // }
-    // return <> {aliveSiblings}</>
-
+    let render = rerendererer()
     return <>
         {
-            allCells.map(cell => {
-                return <div key={cell.address}
-                    onClick={(evt) => {
-                        console.log(`${cell.address} clicked on`)
-                        // if (activeCellArray.includes(cell)) {
-                        //     deadCellArray.push(cell)
-                        //     let cellIndex = activeCellArray.indexOf(cell)
-                        //     activeCellArray.splice(cellIndex, 1)
-                        //     console.log(`Active cells: ${activeCellArray}`)
-                        //     console.log(`Dead cells: ${deadCellArray}`)
-                        // } else {
-                        //     activeCellArray.push(cell)
-                        //     let cellIndex = deadCellArray.indexOf(cell)
-                        //     deadCellArray.splice(cellIndex, 1)
-                        //     console.log(`Active cells: ${activeCellArray}`)
-                        //     console.log(`Dead cells: ${deadCellArray}`)
-                        // } //do we even need to change the div to checked or unchecked now? That was initially intended to give an addressable attribute
-                        // return [
-                        //     evt.target.checked = !evt.target.checked,
-                        //     // console.log(evt), 
-                        //     evt.target.className = activeCellArray.includes(cell) ? "active" : "dead"
-                        // ]
-                    }}
-                    className={cell.status === true ? "active" : cell.status === false ? "dead" : "initialCell"} id={cell.address} value="">
-                    {cell.address}
-                </div>
-            })
+            render
         }
     </>
 }
