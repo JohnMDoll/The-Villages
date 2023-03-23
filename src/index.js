0a4ec0ce0d365626a961cc3b0b04be4b69885fe9
JohnMDoll <johnmdoll3@gmail.com>
2023-03-23 15:32:35 -0500

    modified homepage in package.json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { CapTest } from './CapTest.js';
 // import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import { ApplicationViews } from './views/ApplicationViews';
 
 const container = document.getElementById("root")
@@ -11,9 +11,9 @@ document.body.id = window.location.pathname
 
 const root = createRoot(container)
 root.render(
-  <HashRouter basename="/The-Villages">
+  <BrowserRouter basename="/The-Villages">
     <CapTest />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
