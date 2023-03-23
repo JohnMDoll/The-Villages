46fff1a61e48d5036acb91e1483093fd5294a502
JohnMDoll <johnmdoll3@gmail.com>
2023-03-23 10:10:29 -0500

    pre-server deployment commit

--- src/login/register.js
+++ src/login/register.js
@@ -9,20 +9,20 @@ export const Register = () => {
     const navigate = useNavigate()
     const [userName, setUserName] = useState({ userName: "" })
     const [user, setUser] = useState()
-    // const [password, setPassword] = useState("")
+    const [password, setPassword] = useState("")
 
     const handleRegister = (e) => {
         e.preventDefault()
         DuplicateUserNameCheck(userName)
         const user = JSON.parse(localStorage.getItem("cap_user"))
-        if (user) {setUser(user)}
+        if (user) { setUser(user) }
         navigate("/")
     }
 
     useEffect(
         () => {
-            if (user?.hasOwnProperty("id")) { navigate("/")}
-        },[userName, user]
+            if (user?.hasOwnProperty("id")) { navigate("/") }
+        }, [userName, user]
     )
 
     return (<>
@@ -35,6 +35,9 @@ export const Register = () => {
                     <input className="login--input" id="register" required autoFocus placeholder="username"
                         type="text"
                         onChange={evt => setUserName({ userName: evt.target.value })} />
+                    <input required placeholder="password"
+                        type="password" value={password}
+                        onChange={evt => setPassword(evt.target.value)} />
                     <div className="buttonHolder" id="register">
                         <button type="submit" id="register" onClick={handleRegister}
                         >Identify</button>
